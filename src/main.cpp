59c6c6a45a0bf4dd780ed93e45d2d38e04a45972
Shashwat Ganesh <shashwatganesh@protonmail.com>
2021-10-09 12:51:05 +0530

    Add cairo test function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "lattice.h"
 #include <bits/stdc++.h>
+#include <cairo.h>
 #include <fmt/core.h>
 
 int
@@ -7,7 +8,27 @@ main()
 {
   auto basis = BasisMat{};
   basis << 1, 0, 0, 1;
+  cairo_test();
   find_vertices(5, basis);
+}
+
+auto
+cairo_test() -> void
+{
+  cairo_surface_t* surface =
+    cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 240, 80);
+  cairo_t* cr = cairo_create(surface);
+
+  cairo_select_font_face(
+    cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+  cairo_set_font_size(cr, 32.0);
+  cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+  cairo_move_to(cr, 10.0, 50.0);
+  cairo_show_text(cr, "Hello, world");
+
+  cairo_destroy(cr);
+  cairo_surface_write_to_png(surface, "hello.png");
+  cairo_surface_destroy(surface);
 }
 
 auto
